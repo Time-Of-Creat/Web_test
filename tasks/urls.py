d8f4edc95b5905597812ecdcfd9568f19bf049d5
Time-Of-Creat <matkago12345new@gmail.com>
2026-07-18 13:11:56 +0300

    created task_form page

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.task_list, name="task_list")
+    path("", views.task_list, name="task_list"),
+    path("new/", views.TaskCreateView.as_view(), name="task_create")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from tasks.models import Task
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['title']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task_list')
 
 
 def task_list(request):
