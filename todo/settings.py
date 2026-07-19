5e06a9e79f189a443e601659655055499345138c
Time-Of-Creat <matkago12345new@gmail.com>
2026-07-19 14:56:53 +0300

    fixed bug with login

--- todo/settings.py
+++ todo/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'task-list'
-LOGOUT_REDIRECT_URL = 'task-list'
+LOGIN_REDIRECT_URL = 'task_list'
+LOGOUT_REDIRECT_URL = 'task_list'
