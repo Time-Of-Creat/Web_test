beba9197fdda299640caeba6c2938c36e5356e15
Time-Of-Creat <matkago12345new@gmail.com>
2026-07-19 13:23:44 +0300

    added user model to db

--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from tasks.exceptions import TaskAlreadyCompletedError
+
+
+User = get_user_model()
 
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
